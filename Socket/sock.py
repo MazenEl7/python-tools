43db017423aa85de4b3fcd28c0b5cc8f2a0c4ef7
MazenEl7 <mazenwork9@gmail.com>
2024-10-30 22:44:52 +0300

    Update sock.py

--- Socket/sock.py
+++ Socket/sock.py
@@ -1,15 +1,24 @@
 import socket
+#!!!! To use any of these remove the # before them !!!!!
+
+#To get ip of the website:
+
 #nameEnter = input("Enter the url without www. and .com of the website to get the ip: ")
 #ip = socket.gethostbyname(f"www.{nameEnter}.com")
 
 #print(f"the ip of website is: {ip}")
 #__________________________________________#
+#To get service name of the port number you want to know
+
 #servName = input("Enter the name of the service you want her port: ")
 #portName = socket.getservbyname(servName)
-#
+
 #print(f"The port of the service {servName} is: {portName}")
 #__________________________________________#
-servPortName = int(input("Enter the name of the port you want her service: "))
-servPort = socket.getservbyport(servPortName)
-print(f"The service of the port {servPortName} is: {servPort}")
+#To get port number of the service name you want to know
+
+#servPortName = int(input("Enter the name of the port you want her service: "))
+#servPort = socket.getservbyport(servPortName)
+
+#print(f"The service of the port {servPortName} is: {servPort}")
 #__________________________________________#
